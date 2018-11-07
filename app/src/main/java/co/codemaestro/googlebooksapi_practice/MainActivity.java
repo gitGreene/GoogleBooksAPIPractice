ea2c2c07ddc64590d9076673a57f79491b73564e
Mitch Greene <m.iangreene@gmail.com>
2018-11-06 23:05:56 -0500

    Day 047 of 100DoCC

--- app/src/main/java/co/codemaestro/googlebooksapi_practice/MainActivity.java
+++ app/src/main/java/co/codemaestro/googlebooksapi_practice/MainActivity.java
@@ -1,8 +1,12 @@
 package co.codemaestro.googlebooksapi_practice;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -27,7 +31,32 @@ public class MainActivity extends AppCompatActivity {
     public void searchBooks(View view) {
 
         String queuryString = mBookInput.getText().toString();
-        new FetchBook(mTitleText, mAuthorText).execute(queuryString);
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if(inputManager != null) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+            getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if(connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+        }
+
+        if(networkInfo != null && networkInfo.isConnected() && queuryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queuryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if(queuryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
 
     }
 }
